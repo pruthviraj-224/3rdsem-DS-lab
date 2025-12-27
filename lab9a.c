3eecfb0e8048a0f2874bd69ebb17dec6e7710c93
pruthviraj-224 <pruthviraj.cs24@bmsce.ac.in>
2025-12-27 13:59:34 +0530

    Create lab9a.c

---
+++ lab9a.c
@@ -0,0 +1,48 @@
+#include <stdio.h>
+
+int n;
+int graph[10][10];
+int visited[10];
+int queue[10], front = -1, rear = -1;
+
+/* BFS function */
+void bfs(int start) {
+    int i;
+    visited[start] = 1;
+    queue[++rear] = start;
+
+    while (front != rear) {
+        start = queue[++front];
+        printf("%d ", start);
+
+        for (i = 0; i < n; i++) {
+            if (graph[start][i] == 1 && visited[i] == 0) {
+                visited[i] = 1;
+                queue[++rear] = i;
+            }
+        }
+    }
+}
+
+int main() {
+    int i, j, start;
+
+    printf("Enter number of vertices: ");
+    scanf("%d", &n);
+
+    printf("Enter adjacency matrix:\n");
+    for (i = 0; i < n; i++)
+        for (j = 0; j < n; j++)
+            scanf("%d", &graph[i][j]);
+
+    for (i = 0; i < n; i++)
+        visited[i] = 0;
+
+    printf("Enter starting vertex: ");
+    scanf("%d", &start);
+
+    printf("BFS Traversal: ");
+    bfs(start);
+
+    return 0;
+}
